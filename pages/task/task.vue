0d206a59db2353131b3797176907c8a5dd2f94a6
Chenwuzheng <littlegrass003@gmail.com>
2019-09-23 23:50:18 +0800

    tabbar

--- pages/task/task.vue
+++ pages/task/task.vue
@@ -1,8 +1,26 @@
 <template>
+	<view class="content">
+		<view class="search-view">
+			<m-input class="search-input" type="search" placeholder="请输入姓名" v-model="account"/>
+		</view>
+	</view>
 </template>
 
 <script>
 </script>
 
 <style>
+	.content{
+		background-color: #f7f7f7;
+	}
+	
+	.search-view{
+		width: 100%;
+		height: 96upx;
+		background:rgba(39,179,157,1);
+	}
+	
+	.search-input{
+		background-color: #27B39D;
+	}
 </style>
