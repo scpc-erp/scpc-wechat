47529f92f74c62ab78ee6797f4fe647c32ff9304
Chenwuzheng <littlegrass003@gmail.com>
2019-09-24 11:42:23 +0800

    fix

--- pages/task/task.vue
+++ pages/task/task.vue
@@ -1,45 +1,77 @@
 <template>
 	<view class="content">
-		<view class="search-view">
-			<m-input class="search-input" type="search" placeholder="请输入姓名" v-model="account"/>
-			<button type="primary" @tap="handleClick">点我</button>
+		<view class="top-view">
+			<view class="search-BG-view"> 
+				<image src="../../static/img/task/task-search.png" mode="" class="search-img"></image>
+				<!-- <input class="search-input" type="text" placeholder="搜索任务"/> -->
+				<m-input class="search-input" type="text" placeholder="搜索任务"/>
+			</view>
 		</view>
 	</view>
 </template>
 
 <script>
+	import mInput from '@/components/m-input.vue'
 	export default {
+		components:{
+			mInput
+		},
 		methods:{
 			handleClick() {
 				console.log('12341234');
 				uni.navigateTo({
-					url:'../test/test',
-					animationType: 'pop-in',
-					animationDuration: 200
+					url:'../test/test'
 				});
 			}
 		}
 	}
 </script>
 
-<style>
+<style lang="less">
+	
+	input{
+		font:900 23px;
+	}
 	.content{
 		background-color: #f7f7f7;
 	}
 	
-	.search-view{
+	.top-view{
 		width: 100%;
 		height: 96upx;
 		background:rgba(39,179,157,1);
+		position: relative; 
 	}
 	
-	.search-input{
-		background-color: #27B39D;
+	.search-BG-view{ 
+		height:68upx;
+		background:rgba(255,255,255,0.17);
+		border-radius:35px;
+		border-radius: 35upx;
+		position: absolute;
+		top: 15upx;
+		left: 24upx;
+		right: 24upx;
 	}
 	
-	button{
-		width: 150upx;
-		height: 150upx;
-		background-color: red;
+	.search-img{
+		width: 36upx;
+		height: 36upx;
+		position: absolute;
+		top: 17upx;
+		left: 48upx;
+	}
+	
+	uni-input{
+			position: absolute;
+			height: 100%;
+			width: 80%;
+			left: 90upx;
+			right: 24upx;
+			font-size:28upx !important;
+			line-height: 100%;
+			font-family:PingFangSC-Regular,PingFangSC;
+			font-weight:400; 
+			color:rgba(255,255,255,1) !important; 
 	}
 </style>
--- pages/test/test.vue
+++ pages/test/test.vue
@@ -1,8 +1,23 @@
 <template>
+	<button type="primary" @tap='handleClick'></button>
 </template>
 
 <script>
+	export default {
+		methods:{
+			handleClick() { 
+				uni.navigateBack({
+					delta:1
+				});
+			}
+		}
+	}
 </script>
 
 <style>
+		
+	button{
+		width: 150upx;
+		height: 150upx;
+	}
 </style>
